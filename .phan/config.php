8e72d587972b967933721428a7b1ee4a5bab4f3c
Umherirrender <umherirrender_de.wp@web.de>
2021-04-08 14:55:27 +0200

    build: Do not require extension in composer which is in gerrit
    For the CI it is easier to config the dependency in the CI
    Adjust phan config to be more standard
    Fix a phpcs issue from last merge
    Remove non-functional test class
    Fix spacing in js files to make npm happy
    
    Change-Id: I08561b362a776c4044140676cafd4e6111bac045
    (cherry picked from commit 0be4a9fe9b509ba6a744e21db9a3ba8dafe78dcc)

--- .phan/config.php
+++ .phan/config.php
@@ -1,21 +1,16 @@
 <?php
+
 $cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
 
-$IP = getenv( 'MW_INSTALL_PATH' ) !== false
-	? str_replace( '\\', '/', getenv( 'MW_INSTALL_PATH' ) )
-	: __DIR__ . '/../../..';
-
 $dependencies = [
-		$IP . '/extensions/LDAPAuthentication2',
-		$IP . '/extensions/LDAPProvider',
-		$IP . '/extensions/PluggableAuth',
-		$IP . '/extensions/Renameuser',
-		$IP . '/extensions/UserMerge',
-	];
+	'../../extensions/LDAPAuthentication2',
+	'../../extensions/LDAPProvider',
+	'../../extensions/PluggableAuth',
+	'../../extensions/Renameuser',
+	'../../extensions/UserMerge',
+];
 
 $cfg['directory_list'] = array_merge( $cfg['directory_list'], $dependencies );
-
-$cfg['exclude_file_list'][] = $IP . '/extensions/Renameuser/RenameuserSQL.php';
 
 $cfg['exclude_analysis_directory_list'] = array_merge(
 	$cfg['exclude_analysis_directory_list'], $dependencies

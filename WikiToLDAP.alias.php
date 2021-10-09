c5c37c506e8aaed2ff908eded83da94dd2e748dd
Umherirrender <umherirrender_de.wp@web.de>
2021-10-09 22:59:26 +0200

    Add alias file for special pages
    Change-Id: I18cb424fea4d956d8e1e7356ec98f08c19ec8d6c
    (cherry picked from commit 66eaacaff2cc117a529d216bf9ac7befbc721a5b)

---
+++ WikiToLDAP.alias.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Aliases for special pages
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$specialPageAliases = [];
+
+/** English (English) */
+$specialPageAliases['en'] = [
+	'WikiUserMerge' => [ 'WikiUserMerge' ],
+	'LDAPUserMerge' => [ 'LDAPUserMerge' ],
+];
